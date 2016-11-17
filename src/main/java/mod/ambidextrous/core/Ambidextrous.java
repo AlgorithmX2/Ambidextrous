1e35eab84b7c52a9f810b99f0c5385725e7d7ef5
AlgorithmX2 <AlgorithmX2@gmail.com>
2016-11-16 20:49:41 -0600

    Port to 1.11

--- src/main/java/mod/ambidextrous/core/Ambidextrous.java
+++ src/main/java/mod/ambidextrous/core/Ambidextrous.java
@@ -11,7 +11,7 @@ import net.minecraftforge.fml.relauncher.Side;
 		name = Ambidextrous.MODNAME,
 		modid = Ambidextrous.MODID,
 		version = Ambidextrous.VERSION,
-		acceptedMinecraftVersions = "[1.9.4,1.11)",
+		acceptedMinecraftVersions = "[1.11,1.12)",
 		dependencies = Ambidextrous.DEPENDENCIES )
 public class Ambidextrous
 {
